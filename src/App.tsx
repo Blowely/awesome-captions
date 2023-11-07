1c3f97ae56124e005836fb96ca5bc3c948c889e0
Blowely <maryashin.2014@yandex.ru>
2023-11-07 13:09:22 +0300

    webpack eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import React from "react";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
+  whiteSpace: "nowrap",
+  width: 1
 });
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+        <Button
+          component="label"
+          variant="contained"
+          startIcon={<CloudUploadIcon />}
+        >
           Upload file
           <VisuallyHiddenInput type="file" />
         </Button>
