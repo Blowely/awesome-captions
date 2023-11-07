a8c496fd76a1191423756c27f9e6e3e1c9e75589
Blowely <maryashin.2014@yandex.ru>
2023-11-07 14:59:55 +0300

    extractAudio

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,51 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { Button, message, Upload, type UploadProps } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import type { UploadRequestOption } from "rc-upload/lib/interface";
+import { RcFile } from "antd/es/upload";
 
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1
-});
+const extractAudio = require("ffmpeg-extract-audio");
 
 function App() {
+  const onUploadFile = async (options: UploadRequestOption) => {
+    const { onSuccess, file, onError } = options;
+    // @ts-ignore
+    if (file.type !== "text/csv") {
+      return "lol";
+    }
+
+    await extractAudio({
+      input: "media/1.mp4",
+      output: "test.mp3"
+    });
+  };
+
+  const props: UploadProps = {
+    name: "file",
+    customRequest: onUploadFile,
+    headers: {
+      authorization: "authorization-text"
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          component="label"
-          variant="contained"
-          startIcon={<CloudUploadIcon />}
-        >
-          Upload file
-          <VisuallyHiddenInput type="file" />
-        </Button>
+        <Upload {...props}>
+          <Button icon={<UploadOutlined />}>Click to Upload</Button>
+        </Upload>
       </header>
     </div>
   );
